4d3112d228aaec7ce51935f5a54c9bd54b2084e4
Luca Umekubo <lumekubo1@hwemail.com>
2024-09-20 10:42:13 -0700

    Milestone #1: Initializes the git Repo
    initGitRepo() method that initializes the git repo with objects directory and index file

--- Git.java
+++ Git.java
@@ -1,5 +1,35 @@
-public class Git {
-    public static void main(String[] args) {
-        
+import java.io.File;
+import java.io.IOException;
+
+public class Git{
+    public static void main(String[] args) throws IOException {
+        initGitRepo();
     }
+
+    //makes a Git Repo in the current folder 
+    public static void initGitRepo() throws IOException{
+
+
+        File gitDir = new File("git");
+
+        File objectsDir = new File("git/objects");
+
+        File indexFile = new File("git/index");
+
+        if (gitDir.exists() && objectsDir.exists() && indexFile.exists()){
+            System.out.println("Git Repository already exists");
+        }
+        else{
+            if (!gitDir.exists()){
+                gitDir.mkdir();
+            }
+            if (!objectsDir.exists()){
+                objectsDir.mkdir();
+            }
+            if (!indexFile.exists()){
+                indexFile.createNewFile();
+            }
+        }
+    }
+    
 }
